541f832ca9be74d4a3127253875d1aceb9d00374
Oriol Sarda <alu.oriol.sarda@mataro.epiaedu.cat>
2025-10-09 18:48:42 +0200

    Desenvolupament case 1

--- PE03_oriolsarda.java
+++ PE03_oriolsarda.java
@@ -3,14 +3,42 @@ import java.util.Scanner;
 public class PE03_oriolsarda {
     public static void main(String[] args) {
         Scanner e = new Scanner(System.in);
-        
+        //Variables
+        String opcio1, Nom;
+        int edat;
+        double pes, altura;
+
+        //Menu
         System.out.println("Menu");
         System.out.println("Sisusplau, escull una opcio entre a: Introduccio de dades, b: Modificar dades o c: Visualitzar dades");
-        String opcio1 = e.nextLine();
+        opcio1 = e.nextLine();
 
         switch (opcio1) {
             case "a":
                 System.out.println("Has escollit Introduccio de dades");
+                if (opcio1 == "a")
+                {
+                    System.out.println("Siusplau, introdueix el teu nom complet: ");
+                    Nom = e.nextLine();
+                    System.out.println("Siusplau, introdueix la teva edat: ");
+                    edat = e.nextInt();
+                    if (edat > 120) 
+                    {
+                        System.out.println("Edat incorrecta");
+                    }
+                    System.out.println("Siusplau, introdueix el teu pes en kg: ");
+                    pes = e.nextDouble();
+                    if (pes > 400) 
+                    {
+                        System.out.println("Pes incorrecte");
+                    }  
+                    System.out.println("Siusplau, introdueix la teva altura en metres: ");
+                    altura = e.nextDouble();
+                    if ((altura > 2.5) || (altura < 0.5))
+                    {
+                        System.out.println("Altura incorrecta");
+                    }
+                }   
                 break;
             case "b":
                 System.out.println("Has escollit Modificar dades");
