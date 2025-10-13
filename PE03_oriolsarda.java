6e267370dc7444ebd3df350b58646d403ea31a7c
Oriol Sarda <alu.oriol.sarda@mataro.epiaedu.cat>
2025-10-13 16:54:56 +0200

    Desenvolupament case 1 Versió final

--- PE03_oriolsarda.java
+++ PE03_oriolsarda.java
@@ -1,44 +1,75 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.time.Year;
 
 public class PE03_oriolsarda {
     public static void main(String[] args) {
-        Scanner e = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         //Variables
-        String opcio1, Nom;
+        String opcio1, Nom, pesStr, alturaStr;
         int edat;
         double pes, altura;
 
+        while (true) {
+
         //Menu
         System.out.println("Menu");
-        System.out.println("Sisusplau, escull una opcio entre 1: Introduccio de dades, 2: Modificar dades, 3: Visualitzar dades o 4: Sortir");
-        opcio1 = e.nextLine();
+        System.out.println("Sisusplau, escull una opcio entre:  ");
+        System.out.println("1: Introduccio de dadesades");
+        System.out.println("2: Modificar dades");
+        System.out.println("3: Visualitzar dades");
+        System.out.println("4: Sortir");
+        opcio1 = scanner.nextLine();
 
         switch (opcio1) {
             case "1":
                 System.out.println("Has escollit Introduccio de dades");
-                if (opcio1 == "1")
+                try
                 {
+                    //Nom
                     System.out.println("Siusplau, introdueix el teu nom complet: ");
-                    Nom = e.nextLine();
-                    System.out.println("Siusplau, introdueix la teva edat: ");
-                    edat = e.nextInt();
-                    if (edat > 120) 
+                    Nom = scanner.nextLine();
+                    if (Nom.equals(""))
                     {
-                        System.out.println("Edat incorrecta");
+                        System.out.println("Nom incorrecte");
+                        break;
                     }
-                    System.out.println("Siusplau, introdueix el teu pes en kg: ");
-                    pes = e.nextDouble();
-                    if (pes > 400) 
+                    //Edat
+                    System.out.println("Siusplau, introdueix la teva edat: ");
+                    edat = Integer.parseInt(scanner.nextLine());
+                    if (edat > 120 || edat < 0)
                     {
-                        System.out.println("Pes incorrecte");
+                        System.out.println("Error: L'edat ha de ser un enter posistiu positiu mes petit o igual que 120");
+                        break;
+                    }
+                    //Pes
+                    System.out.println("Siusplau, introdueix el teu pes en kg: ");
+                    pesStr = scanner.nextLine().replace(',', '.');
+                    pes = Double.parseDouble(pesStr);
+                    if ((pes > 400) || (pes <= 0))
+                    {
+                        System.out.println("Error: El pes no pot ser major que 400 kg");
+                        break;
                     }  
+                    //Altura
                     System.out.println("Siusplau, introdueix la teva altura en metres: ");
-                    altura = e.nextDouble();
-                    if ((altura > 2.5) || (altura < 0.5))
+                    alturaStr = scanner.nextLine().replace(',', '.');
+                    altura = Double.parseDouble(alturaStr);
+                    if ((altura >= 2.5) || (altura <= 0.5))
                     {
                         System.out.println("Altura incorrecta");
                     }
-                }   
+                }
+                    catch (NumberFormatException e)
+                    {
+                        System.out.println("Error: format numèric invàlid");
+                        System.out.println(e.getMessage());
+                    }
+                    catch (Exception e)
+                    {
+                        System.out.println("Error inesperat");
+                        System.out.println(e.getMessage());
+                    }
                 break;
             case "2":
                 System.out.println("Has escollit Modificar dades");
@@ -52,6 +83,7 @@ public class PE03_oriolsarda {
             default:
                 System.out.println("Opcio incorrecta");
                 break;
+            }
         }
     }
 }
